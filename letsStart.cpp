e3f4cda4cd004a62c0c55b93b6333524122738a5
Serenhov <madeleine.serenhov@gmail.com>
2017-03-01 22:41:07 +0100

    Start!

---
+++ letsStart.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Madeleine Serenhov on 2017-03-01.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    std::cout << "Hello, group!" << endl;
+    return 0;
+
+}
